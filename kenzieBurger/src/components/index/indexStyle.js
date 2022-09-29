bf609cbc83cc404071c186020ca4d08c8309e1a9
Matheus Soares <matheusfs774@gmail.com>
2022-09-29 14:07:54 -0400

    add: add header html and css

---
+++ kenzieBurger/src/components/index/indexStyle.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+
+export const Div = styled.div`
+    width: 100%;
+    max-width: 1600px;
+
+    margin: 0 auto;
+    padding: 10px;`
+
+export const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    form{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        width: 100%;
+        max-width: 300px;
+
+        padding: 10px;
+
+        border: 2px solid var(--gray3);
+        border-radius: 8px;
+
+        input{
+            max-width: 120px;
+            
+            border: none;
+
+            color: var(--gray3);
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+        }
+
+        input:focus{
+            outline: none;
+            color: var(--gray2)
+        }
+
+        button{
+            width: 100px;
+            height: 30px;
+
+            background-color: var(--primary);
+            color: #FFFFFF;
+
+            border: 2px solid var(--primary);
+            border-radius: 8px;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 500;
+        }
+
+        button:hover{
+            filter: brightness(0.9);
+            cursor: pointer;
+        }
+    }
+    form:hover{
+        border: 2px solid var(--gray1);
+    }
+`
